082877a3cccc190bc87b29dff474f2429b007640
abcp8844-source <abcp8844@gmail.com>
2026-06-20 09:02:08 +0700

    Update facebook_poster.py

--- facebook_poster.py
+++ facebook_poster.py
@@ -3,7 +3,6 @@ import json
 import requests
 
 PAGE_ID = "514947098373834"
-GROUP_ID = "1403757117731031"
 ACCESS_TOKEN = os.environ.get("PAGE_ACCESS_TOKEN")
 FILE_PATH = "pending_posts.json"
 
@@ -40,8 +39,8 @@ def post_to_facebook():
 
     message = f"{title}\n\n\n{brand_intro}\n\n👉 Apply Here: {link}{hashtags}"
 
+    
     page_url = f"https://graph.facebook.com/v19.0/{PAGE_ID}/feed"
-    group_url = f"https://graph.facebook.com/v19.0/{GROUP_ID}/feed"
     
     payload = {
         "message": message,
@@ -54,17 +53,7 @@ def post_to_facebook():
 
         if "id" in res_data:
             print(f"Successfully posted to Facebook Page! Post ID: {res_data['id']}")
-            
-            try:
-                group_response = requests.post(group_url, data=payload)
-                group_res_data = group_response.json()
-                if "id" in group_res_data:
-                    print(f"Successfully posted to Facebook Group! Post ID: {group_res_data['id']}")
-                else:
-                    print(f"Facebook Group API Error: {group_res_data}")
-            except Exception as group_err:
-                print(f"Error during group posting: {group_err}")
-
+            # پوسٹ کامیاب ہونے پر لسٹ سے ہٹائیں
             posts.pop(0)
             with open(FILE_PATH, "w", encoding="utf-8") as f:
                 json.dump(posts, f, indent=2, ensure_ascii=False)
